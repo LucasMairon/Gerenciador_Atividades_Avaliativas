298acf2a0e0e2a7bb1b650aa3a901aea7d3be08b
LucasMairon <lucasmairon2@gmail.com>
2025-10-10 23:19:03 -0300

    feat: adding subjective question

--- backend/question/models/__init__.py
+++ backend/question/models/__init__.py
@@ -1,7 +1,9 @@
 from .question import Question
 from .objective_question import ObjectiveQuestion
+from .subjective_question import SubjectiveQuestion
 
 __all__ = [
     Question,
     ObjectiveQuestion,
+    SubjectiveQuestion,
 ]
---
+++ backend/question/models/subjective_question.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .question import Question
+
+
+class SubjectiveQuestion(Question):
+    expected_answer = models.CharField(
+        verbose_name=_('Expected answer'),
+        max_length=500
+    )
+    key_answers = models.CharField(
+        verbose_name=_('Key answers'),
+        max_length=100
+    )
+
+    def __str__(self):
+        return self.statement
+
+    class Meta:
+        verbose_name = 'Questão Subjetiva'
+        verbose_name_plural = 'Questões Subjetivas'
