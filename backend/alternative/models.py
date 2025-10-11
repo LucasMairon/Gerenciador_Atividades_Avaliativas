3674c282693bcae597fa57c07a53fcefa348bb28
LucasMairon <lucasmairon2@gmail.com>
2025-10-10 22:56:31 -0300

    feat: adding objective question relation to alternative

--- backend/alternative/models.py
+++ backend/alternative/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from question.models import ObjectiveQuestion
 import uuid
 
 
@@ -17,6 +18,11 @@ class Alternative(models.Model):
     is_correct = models.BooleanField(
         verbose_name=_('Is Correct'),
         default=False
+    )
+    question = models.ForeignKey(
+        ObjectiveQuestion,
+        on_delete=models.CASCADE,
+        related_name=_('Alternatives')
     )
 
     def __str__(self):
