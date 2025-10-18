ec7c42d084bcd1c878aa1e66b721bbec2198cd7a
LucasMairon <lucasmairon2@gmail.com>
2025-10-17 23:18:49 -0300

    remove: removing unnecessary validations

--- backend/question/models/objective_question.py
+++ backend/question/models/objective_question.py
@@ -1,10 +1,6 @@
 from django.db import models
-from django.forms import ValidationError
 from django.utils.translation import gettext_lazy as _
 from .question import Question
-
-MAX_NUMBER_ALTERNATIVES = 5
-MIN_NUMBER_ALTERNATIVES = 4
 
 
 class ObjectiveQuestion(Question):
@@ -15,16 +11,6 @@ class ObjectiveQuestion(Question):
 
     def __str__(self):
         return self.statement
-
-    def clean(self):
-        super().clean()
-
-        if self.pk:
-            alternatives_count = self.alternatives.count()
-            if alternatives_count > MAX_NUMBER_ALTERNATIVES and alternatives_count < MIN_NUMBER_ALTERNATIVES:
-                raise ValidationError(
-                    f'Número de alternativas inválidas, por favor coloque o número de alternativas entre ${MAX_NUMBER_ALTERNATIVES} e ${MIN_NUMBER_ALTERNATIVES}, atualmente possui ${alternatives_count}'
-                )
 
     class Meta:
         verbose_name = 'Questão Objetiva'
