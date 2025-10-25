fcbdb7c928ed0417d892d6b519926642013edfea
LucasMairon <lucasmairon2@gmail.com>
2025-10-24 23:51:31 -0300

    feat: adding decimal_to_letter template tag

--- backend/question/templatetags/question_template_utils.py
+++ backend/question/templatetags/question_template_utils.py
@@ -11,3 +11,10 @@ def add_param_to_url(context, **kwargs):
         query_params[key] = value
 
     return query_params.urlencode()
+
+
+@register.simple_tag()
+def decimal_to_letter(index):
+    ascii_value = 65 + index
+
+    return chr(ascii_value)
