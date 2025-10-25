8a97265d60cfbfcbbffd9458b54242313baddd6a
LucasMairon <lucasmairon2@gmail.com>
2025-10-25 01:00:50 -0300

    feat(form): improving description placeholder text

--- backend/alternative/forms.py
+++ backend/alternative/forms.py
@@ -1,17 +1,27 @@
 from django import forms
 from .models import Alternative
+from question.templatetags.question_template_utils import decimal_to_letter
 
 
 class AlternativeForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.prefix:
+            index = int(self.prefix.split('-')[-1])
+            letter = decimal_to_letter(index + 1)
+            placeholder_text = f'Digite a alternativa {letter}'
+            self.fields['description'].widget.attrs['placeholder'] = placeholder_text
+
     class Meta:
         model = Alternative
-        fields = ['description', 'is_correct', 'order']
+        fields = ['description', 'is_correct']
 
         widgets = {
             'description': forms.Textarea(
                 attrs={
                     'class': 'form-control',
-                    'placeholder': 'teste',
                     'rows': 1
                 }
             ),
