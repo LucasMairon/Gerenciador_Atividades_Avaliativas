82599c18f880361efc38a85408b6ec5a157f0a8a
LucasMairon <lucasmairon2@gmail.com>
2025-10-25 01:01:14 -0300

    fix: fixing default value to ascii numbers

--- backend/question/templatetags/question_template_utils.py
+++ backend/question/templatetags/question_template_utils.py
@@ -15,6 +15,6 @@ def add_param_to_url(context, **kwargs):
 
 @register.simple_tag()
 def decimal_to_letter(index):
-    ascii_value = 65 + index
+    ascii_value = 64 + index
 
     return chr(ascii_value)
