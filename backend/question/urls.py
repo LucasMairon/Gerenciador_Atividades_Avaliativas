6b306f64f591b750235fb8e1ccb1e966ade3dd68
LucasMairon <lucasmairon2@gmail.com>
2025-10-25 15:17:32 -0300

    feat(urls): adding question delete url

--- backend/question/urls.py
+++ backend/question/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
          views.QuestionCreateView.as_view(), name='create'),
     path('list/', views.QuestionListView.as_view(), name='list'),
     path("update/<str:type>/<uuid:pk>/",
-         views.QuestionUpdateView.as_view(), name="update")
+         views.QuestionUpdateView.as_view(), name="update"),
+    path('delete/<uuid:pk>/', views.QuestionDeleteView.as_view(), name='delete')
 ]
