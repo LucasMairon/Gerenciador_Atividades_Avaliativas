7cf822b9532b4f28b7c500e61699f9239bd62613
LucasMairon <lucasmairon2@gmail.com>
2025-10-24 23:48:39 -0300

    feat(style): adding style format to alternative form

--- backend/alternative/forms.py
+++ backend/alternative/forms.py
@@ -5,4 +5,19 @@ from .models import Alternative
 class AlternativeForm(forms.ModelForm):
     class Meta:
         model = Alternative
-        fields = ['description', 'is_correct']
+        fields = ['description', 'is_correct', 'order']
+
+        widgets = {
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'teste',
+                    'rows': 1
+                }
+            ),
+            'is_correct': forms.CheckboxInput(
+                attrs={
+                    'class': 'form-check-input mt-0'
+                }
+            )
+        }
