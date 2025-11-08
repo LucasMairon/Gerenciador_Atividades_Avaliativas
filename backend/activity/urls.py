4036fb50f15f30ea615d95ce50352c0c856601b0
LucasMairon <lucasmairon2@gmail.com>
2025-11-08 00:12:45 -0300

    fix: fixing file preview url

--- backend/activity/urls.py
+++ backend/activity/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('list/', views.ActivityListView.as_view(), name='list'),
     path('create/', views.ActivityCreateView.as_view(), name='create'),
     path('delete/<uuid:pk>/', views.ActivityDeleteView.as_view(), name='delete'),
-    path('preview/pdf/<uuid:pk>/',
-         views.ActivityPDFPreviewView.as_view(), name='export'),
+    path('preview/<uuid:pk>/',
+         views.ActivityPDFPreviewView.as_view(), name='preview'),
 ]
