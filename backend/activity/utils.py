b8ea056268d68cf2672fef05e4c4bd0ab778ed49
LucasMairon <lucasmairon2@gmail.com>
2025-11-09 23:31:14 -0300

    feat: adding queryset param to get question filter method

--- backend/activity/utils.py
+++ backend/activity/utils.py
@@ -3,9 +3,12 @@ from activity.filters import QuestionActivityFilterSet
 from django.db.models import Q
 
 
-def get_question_activity_filter(owner, request):
-    questions_queryset = Question.objects.filter(
-        Q(visibility=True) | Q(owner=owner)).order_by('-updated_at', '-created_at')
+def get_question_activity_filter(owner, request, queryset):
+    if queryset:
+        questions_queryset = queryset
+    else:
+        questions_queryset = Question.objects.filter(
+            Q(visibility=True) | Q(owner=owner)).order_by('-updated_at', '-created_at')
 
     question_activity_filterset = QuestionActivityFilterSet(
         request.GET or None,
