5e62b0ca78fddc7614bbebddb81c0ae769a17e3a
LucasMairon <lucasmairon2@gmail.com>
2025-11-17 14:24:21 -0300

    feat: adding login url and view

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from .forms import UserLoginForm
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    authentication_form = UserLoginForm
+    template_name = 'user/login.html'
