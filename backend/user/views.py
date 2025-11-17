2b7f7e3e7beb749a5c724a905cc1c1d883daf54a
LucasMairon <lucasmairon2@gmail.com>
2025-11-17 14:40:24 -0300

    feat: adding logout view

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,11 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from .forms import UserLoginForm
 
 
 class UserLoginView(LoginView):
     authentication_form = UserLoginForm
     template_name = 'user/login.html'
+
+
+class UserLogoutView(LogoutView):
+    next_page = 'user:login'
