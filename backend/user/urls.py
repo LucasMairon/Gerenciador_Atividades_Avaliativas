1018fc279be322bee0f545f05183b32693ea8a10
LucasMairon <lucasmairon2@gmail.com>
2025-11-20 14:49:19 -0300

    feat: adding password views and urls

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -5,5 +5,9 @@ app_name = 'user'
 
 urlpatterns = [
     path('', views.UserLoginView.as_view(), name='login'),
-    path('user/logout/', views.UserLogoutView.as_view(), name='logout')
+    path('user/logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('user/password_reset/',
+         views.UserPasswordResetView.as_view(), name='password_reset'),
+    path('user/password_reset_confirm/<uidb64>/<token>/',
+         views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from .forms import UserLoginForm
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetConfirmView
+from django.urls import reverse_lazy
+from .forms import UserLoginForm, UserPasswordResetForm, UserSetPasswordForm
 
 
 class UserLoginView(LoginView):
@@ -9,3 +10,17 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     next_page = 'user:login'
+
+
+class UserPasswordResetView(PasswordResetView):
+    template_name = 'user/password_reset.html'
+    email_template_name = 'user/partials/password_reset_email.html'
+    html_email_template_name = 'user/partials/password_reset_email.html'
+    success_url = reverse_lazy('user:login')
+    form_class = UserPasswordResetForm
+
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'user/password_reset_confirm.html'
+    success_url = reverse_lazy('user:login')
+    form_class = UserSetPasswordForm
