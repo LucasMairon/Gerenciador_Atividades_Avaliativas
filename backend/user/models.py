33f36b67b77d51ebca3bf508f8e155d604cdc6d6
LucasMairon <lucasmairon2@gmail.com>
2025-11-22 14:46:33 -0300

    fix: adding unique validation to personal email

--- backend/user/models.py
+++ backend/user/models.py
@@ -23,7 +23,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         max_length=255,
         blank=True,
         null=True,
-        validators=[validators.personal_email_validator]
+        validators=[validators.personal_email_validator],
+        unique=True
     )
     institutional_email = models.CharField(
         verbose_name=_('Institutional Email'),
