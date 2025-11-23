cfe39dda6cb8177e5e922da5c75e055954a88f7d
fernandoalvess <fernandoumalves@gmail.com>
2025-11-23 00:46:03 -0300

    feat: implementing HTMX on the login screen

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,11 +1,24 @@
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetConfirmView
 from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.contrib.auth import login
 from .forms import UserLoginForm, UserPasswordResetForm, UserSetPasswordForm
 
 
 class UserLoginView(LoginView):
     authentication_form = UserLoginForm
     template_name = 'user/login.html'
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+
+        if self.request.htmx:
+            response = HttpResponse()
+            success_url = self.get_success_url()
+            response['HX-Redirect'] = success_url
+            return response
+        
+        return super().form_valid(form)
 
 
 class UserLogoutView(LogoutView):
